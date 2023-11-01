bbe7f2eea421e0f0417eb86e351bdcb603b04c1e
Lotus-YR <2070048058@qq.com>
2023-11-01 23:54:00 +0800

    User

---
+++ User.java
@@ -0,0 +1,28 @@
+public class User {
+    String name;
+   private String password;
+   private double money;
+
+    User(String nm,String pw,double m){
+        name=nm;
+        password=pw;
+        money=m;
+    }
+    User(){
+        name=null;
+        password=null;
+        money=0;
+    }
+    String GetPassword(){
+        return password;
+    }
+    double GetMoney(){
+        return money;
+    }
+    void SetMoney(char p,double d){
+        if(p=='+') money+=d;
+        else if(p=='-')money-=d;
+        else  System.out.println("Wrong order!");
+    }
+}
+
