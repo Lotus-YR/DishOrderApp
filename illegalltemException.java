76a18c3d81ff5bec11a444c95a85cb66caafc2b4
Lotus-YR <2070048058@qq.com>
2023-11-04 14:51:23 +0800

    用户余额不足

--- illegalltemException.java
+++ illegalltemException.java
@@ -1,11 +1,13 @@
-import java.io.*;
-
+//检测用户金额是否足够
 public class illegalltemException extends Exception{
     private double amount;
     public illegalltemException(double amount) {
         this.amount=-amount;
     }
+
     public double GetAmount(){
         return amount;
     }
+
 }
+
